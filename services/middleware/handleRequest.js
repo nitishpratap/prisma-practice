55433d6fcdf724023e6b69a1cf07127f24381fa4
nitishpratap18 <nitish@itinker.io>
2024-08-07 16:59:03 +0530

    showing to closest

--- services/middleware/handleRequest.js
+++ services/middleware/handleRequest.js
@@ -7,6 +7,6 @@ const handleRequest = (app)=>{
 
 module.exports = handleRequest;
 /**
- * Adding comment in dev
- * suppose this is code in dev and i need to cherry pick this in prod
+ * Hello Closest
+ * How are you?
  */
